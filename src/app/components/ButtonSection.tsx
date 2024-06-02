feeb1634e918d579796b39ef04a89871378610e1
TiThuthur <thurthur.petit.py@gmail.com>
2024-06-02 21:26:06 +0200

    changement du body en slate 900

--- src/app/components/ButtonSection.tsx
+++ src/app/components/ButtonSection.tsx
@@ -1,14 +1,45 @@
-import { Button,Space } from "antd"
+import { Button, Space } from "antd";
 
-function ButtonSection(){
-    const alphabet=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    return (
-        <Space size={[26,16]} wrap className="m-8 rounded border border-slate-600 p-6">
-            {alphabet.map((_,index)=>(
-                <Button key={index}>{alphabet[index]}</Button>
-            ))}
-        </Space>
-    )
+function ButtonSection() {
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  return (
+    <Space
+      size={[26, 16]}
+      wrap
+      className="m-8 rounded border border-slate-600 p-6"
+    >
+      {alphabet.map((_, index) => (
+        <Button key={index}>{alphabet[index]}</Button>
+      ))}
+    </Space>
+  );
 }
 
-export default ButtonSection
+export default ButtonSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import ScoreSection from "./components/Score section";
 import ButtonSection from "./components/ButtonSection";
 
 export default function Home() {
+  
   return (
-    <>
+    <body className="bg-slate-900">
       <header>
-        <h1 className="text-center text-4xl">The Hanged-man game</h1>
+        <h1 className="text-center text-4xl text-slate-50">The Hanged-man game</h1>
       </header>
       <main className="flex flex-col items-center justify-between p-24">
         <section className="flex w-full flex-col justify-around md:flex-row">
@@ -16,6 +17,6 @@ export default function Home() {
         </section>
         <ButtonSection/>
       </main>
-    </>
+    </body>
   );
 }
