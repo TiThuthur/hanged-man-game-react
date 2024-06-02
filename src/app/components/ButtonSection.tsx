69547419c203d75826cd1eebe1954cd5a765b3c8
arthur <Tithuthur@mbp-de-emeraude.home>
2024-06-02 13:22:36 +0200

    ajout des lettres des boutons

--- src/app/components/ButtonSection.tsx
+++ src/app/components/ButtonSection.tsx
@@ -1,10 +1,10 @@
 import { Button,Space } from "antd"
 
 function ButtonSection(){
-    const alphabet=["A","B","C","D","E","F","G","H","I"]
+    const alphabet=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     return (
-        <Space size={[26,16]} wrap>
-            {new Array(9).fill(null).map((_,index)=>(
+        <Space size={[26,16]} wrap className="m-8 rounded border border-slate-600 p-6">
+            {alphabet.map((_,index)=>(
                 <Button key={index}>{alphabet[index]}</Button>
             ))}
         </Space>
